4fd20e8c3d3fb4a263d7bdb8f0f9cda7f368477b
Hiroaki KAWAI <hiroaki.kawai@gmail.com>
2014-11-06 16:56:23 +0900

    bump-up version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ except:
 	from distutils.core import setup
 
 setup(name="pical",
-	version="0.1",
+	version="0.2",
 	author="Hiroaki KAWAI",
 	author_email="hiroaki.kawai+pypi@gmail.com",
 	url="https://github.com/hkwi/pical",

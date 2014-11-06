117183506f8944f189758f53fc530d8784e76aba
Hiroaki KAWAI <hiroaki.kawai@gmail.com>
2014-11-06 10:18:57 +0900

    add rfc4791 example test

--- tests/sample.py
+++ tests/sample.py
@@ -11,62 +11,92 @@ logging.basicConfig()
 
 def test_caldav_7_8_1():
 	for f in glob.glob("%s/rfc4791_b_*.ics" % os.path.dirname(__file__)):
-		print("**** %s ****" % f)
 		for c in pical.parse(open(f,"rb")):
 			# <C:time-range start="20060104T000000Z" end="20060105T000000Z"/>
 			u = c.time_range(time_range=[datetime(2006,1,4,tzinfo=pical.utc), datetime(2006,1,5,tzinfo=pical.utc)])
-			if u.name != "VCALENDAR":
-				continue
+			assert u.name == "VCALENDAR"
+			f2 = f.replace("_b_","_7_8_1_")
 			if len([c for c in u.children if c.name=="VEVENT"]):
-				for l in u.serialize():
-					print(l)
+				e = u.clone()
+				e.children = []
+				e.properties = []
+				for name,value,params in u.properties:
+					if name == "VERSION":
+						e.properties.append((name,value,params))
+				for c in u.children:
+					if c.name == "VEVENT":
+						props = []
+						for name,value,params in c.properties:
+							if name in "SUMMARY UID DTSTART DTEND DURATION RRULE RDATE EXRULE EXDATE RECURRENCE-ID".split():
+								props.append((name,value,params))
+						c.properties = props
+						e.children.append(c)
+					elif c.name == "VTIMEZONE":
+						e.children.append(c)
+				
+				assert e==pical.parse(open(f2,"rb"))[0], "result %s mismatch" % f2
+			else:
+				assert not os.path.exists(f2)
 
 def test_caldav_7_8_2():
 	for f in glob.glob("%s/rfc4791_b_*.ics" % os.path.dirname(__file__)):
-		print("**** %s ****" % f)
 		for c in pical.parse(open(f,"rb")):
 			# <C:limit-recurrence-set start="20060103T000000Z" end="20060105T000000Z"/>
-			u = c.time_range(recur=[datetime(2006,1,3,tzinfo=pical.utc), datetime(2006,1,5,tzinfo=pical.utc)])
-			if u.name != "VCALENDAR":
-				continue
+			u = c.time_range(recur=[datetime(2006,1,3,tzinfo=pical.utc), datetime(2006,1,5,tzinfo=pical.utc)], component="VEVENT")
+			assert u.name == "VCALENDAR"
+			f2 = f.replace("_b_","_7_8_2_")
 			if len([c for c in u.children if c.name=="VEVENT"]):
-				for l in u.serialize():
-					print(l)
+				assert u==pical.parse(open(f2,"rb"))[0], "result %s mismatch" % f2
+			else:
+				assert not os.path.exists(f2)
 
 def test_caldav_7_8_3():
 	for f in glob.glob("%s/rfc4791_b_*.ics" % os.path.dirname(__file__)):
-		print("**** %s ****" % f)
 		for c in pical.parse(open(f,"rb")):
 			# <C:expand start="20060103T000000Z" end="20060105T000000Z"/>
-			u = c.time_range(expand=[datetime(2006,1,3,tzinfo=pical.utc), datetime(2006,1,5,tzinfo=pical.utc)])
-			if u.name != "VCALENDAR":
-				continue
+			u = c.time_range(expand=[datetime(2006,1,3,tzinfo=pical.utc), datetime(2006,1,5,tzinfo=pical.utc)], component="VEVENT")
+			assert u.name == "VCALENDAR"
+			f2 = f.replace("_b_","_7_8_3_")
 			if len([c for c in u.children if c.name=="VEVENT"]):
-				for l in u.clone(in_utc=True).serialize():
-					print(l)
+				e = u.clone(in_utc=True)
+				assert e==pical.parse(open(f2,"rb"))[0], "result %s mismatch" % f2
+			else:
+				assert not os.path.exists(f2)
 
 def test_caldav_7_8_4():
 	for f in glob.glob("%s/rfc4791_b_*.ics" % os.path.dirname(__file__)):
-		print("**** %s ****" % f)
 		for c in pical.parse(open(f,"rb")):
 			# <C:limit-freebusy-set start="20060102T000000Z" end="20060103T000000Z"/>
-			u = c.time_range(time_range=[datetime(2006,1,2,tzinfo=pical.utc), datetime(2006,1,3,tzinfo=pical.utc)])
-			if u.name != "VCALENDAR":
-				continue
+			u = c.time_range(time_range=[datetime(2006,1,2,tzinfo=pical.utc), datetime(2006,1,3,tzinfo=pical.utc)], component="VFREEBUSY")
+			assert u.name == "VCALENDAR"
+			f2 = f.replace("_b_","_7_8_4_")
 			if len([c for c in u.children if c.name=="VFREEBUSY"]):
-				for l in u.time_filter(fb_range=[datetime(2006,1,2,tzinfo=pical.utc), datetime(2006,1,3,tzinfo=pical.utc)]).serialize():
-					print(l)
+				u = u.time_filter(fb_range=[datetime(2006,1,2,tzinfo=pical.utc), datetime(2006,1,3,tzinfo=pical.utc)])
+				e = u.clone()
+				e.children = []
+				for c in u.children:
+					if c.name == "VFREEBUSY":
+						e.children.append(c)
+				assert e==pical.parse(open(f2,"rb"))[0], "result %s mismatch" % f2
+			else:
+				assert not os.path.exists(f2)
 
 def test_caldav_7_8_5():
 	for f in glob.glob("%s/rfc4791_b_*.ics" % os.path.dirname(__file__)):
-		print("**** %s ****" % f)
 		for c in pical.parse(open(f,"rb")):
 			# <C:time-range start="20060106T100000Z" end="20060107T100000Z"/>
 			u = c.time_range(alarm_range=[datetime(2006,1,6,10,0,0,tzinfo=pical.utc), datetime(2006,1,7,10,0,0,tzinfo=pical.utc)], component="VTODO")
-			if len([c for c in u.children if c.name!="VTIMEZONE"])==0:
-				continue
-			for l in u.serialize():
-				print(l)
+			assert u.name == "VCALENDAR"
+			f2 = f.replace("_b_","_7_8_5_")
+			if len([c for c in u.children if c.name=="VTODO"]):
+				e = u.clone()
+				e.children = []
+				for c in u.children:
+					if c.name == "VTODO":
+						e.children.append(c)
+				assert e==pical.parse(open(f2,"rb"))[0], "result %s mismatch" % f2
+			else:
+				assert not os.path.exists(f2)
 
 def test_parse():
 	for f in glob.glob("%s/*.ics" % os.path.dirname(__file__)):
@@ -79,8 +109,6 @@ def test_range():
 	q = cal.time_range(expand=[datetime(2015,4,1,tzinfo=pical.utc), datetime(2015,6,1,tzinfo=pical.utc)])
 	assert len([c for c in q.children if c.name=="VEVENT"])==10
 	q = cal.time_range(time_range=[datetime(2015,4,1,tzinfo=pical.utc), datetime(2015,6,1,tzinfo=pical.utc)])
-	for l in q.serialize():
-		print(l)
 	assert len([c for c in q.children if c.name=="VEVENT"])==3
 
 def test_eq():
@@ -96,13 +124,8 @@ if __name__=="__main__":
 	test_eq()
 	test_parse()
 	test_range()
-	print("### 4791 7.8.1 ###")
 	test_caldav_7_8_1()
-	print("### 4791 7.8.2 ###")
 	test_caldav_7_8_2()
-	print("### 4791 7.8.3 ###")
 	test_caldav_7_8_3()
-	print("### 4791 7.8.4 ###")
 	test_caldav_7_8_4()
-	print("### 4791 7.8.5 ###")
 	test_caldav_7_8_5()
